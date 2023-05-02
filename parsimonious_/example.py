37fa5c2d92e9b57baf900bf2f4995a68d6914086
xnuinside <xnuinside@gmail.com>
2023-05-02 09:40:34 +0300

    fix parsiminious grammar

--- parsimonious_/example.py
+++ parsimonious_/example.py
@@ -2,16 +2,19 @@ import parsimonious
 
 # Define the grammar for the key-value pairs
 grammar = """
-line = key colon value
+line = key_expr colon value_expr
+key_expr =  key (op key)?
+value_expr = value (op value)?
 key = ~"[a-zA-Z_][a-zA-Z0-9_]*"
-colon = ":"
+value = date / float / int /  date_range
+colon = ":" ws?
 float = ~"\d+\.\d+"
 int = ~"\d+"
 date = ~"\d{4}-\d{2}-\d{2}"
 date_range = date "/" date
-value = float / int / date / date_range / expr
-expr = value op value
-op = ("&" / "-")
+op =  ws? ("&" / "-"/ "/")  ws? 
+ws          = ~"\s*"
+emptyline   = ws+
 """
 
 # Define the actions for the parser
@@ -42,6 +45,10 @@ class LineVisitor(parsimonious.NodeVisitor):
     
     def visit_op(self, node, visited_children):
         return node.text
+    
+    def generic_visit(self, node, visited_children):
+        """ The generic visit method. """
+        return visited_children or node
 
 # Parse the lines and return the results as a list of tuples
 lines = """clients: 23.05
@@ -53,6 +60,8 @@ cats & orders: 2019-10-10
 cats - orders: 2024-12-01 / 2025-12-01"""
 
 parser = parsimonious.Grammar(grammar)
+
+    
 visitor = LineVisitor()
 
 results = []
